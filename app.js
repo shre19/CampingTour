dbd7ccf8e086a9bd32f2e50f5b26b38e46122a18
shre19 <shreyasmohite2001@gmail.com>
2021-12-10 09:32:30 +0530

    Finished with the project

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const methodOverride = require('method-override');
 const passport = require('passport')
 const LocalStrategy  = require('passport-local')
 const User = require('./models/user')
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet')
 
 
 const userRoutes = require('./routes/users')
@@ -44,19 +46,23 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize());
 
 const sessionConfig = {
+    name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        //secure: true,
         expires: Date.now() + 1000*60*60*24*7,
         maxAge: 1000*60*60*24*7
     }
 }
 app.use(session(sessionConfig))
 app.use(flash())
+app.use(helmet({contentSecurityPolicy: false}))
 
 app.use(passport.initialize())
 app.use(passport.session())
